b4f1962d43c5e48e554cafb170b4325a995db375
Michael Stapelberg <michael@stapelberg.de>
2025-02-22 08:09:57 +0100

    ondeviceapi: add BuildTimestamp to IndexResult

--- ondeviceapi/model_index_result.go
+++ ondeviceapi/model_index_result.go
@@ -16,6 +16,8 @@ type IndexResult struct {
 	Kernel string `json:"Kernel,omitempty"`
 	// Model name of the machine on which gokrazy is running
 	Model string `json:"Model,omitempty"`
+	// Build timestamp of the running gokrazy instance
+	BuildTimestamp string `json:"BuildTimestamp,omitempty"`
 	// running services
 	Services []Service `json:"Services,omitempty"`
 }
