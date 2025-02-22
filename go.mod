e10605df0220a882a98ec4ff0f25c59fdf6e79e2
Michael Stapelberg <michael@stapelberg.de>
2025-02-22 08:24:04 +0100

    remove dependency on gokrazy/gokrazy just for socket path
    We will need to manually update the socket path here if we change it,
    but that is fine — we will most likely need a fallback anyway.

--- go.mod
+++ go.mod
@@ -4,15 +4,5 @@ go 1.24
 
 require (
 	github.com/antihax/optional v1.0.0
-	github.com/gokrazy/gokrazy v0.0.0-20250222061409-bd0bb5f1d0b5
 	golang.org/x/oauth2 v0.23.0
-)
-
-require (
-	github.com/gokrazy/internal v0.0.0-20250214203001-b1610a6e7271 // indirect
-	github.com/google/renameio/v2 v2.0.0 // indirect
-	github.com/kenshaw/evdev v0.1.0 // indirect
-	github.com/mdlayher/watchdog v0.0.0-20201005150459-8bdc4f41966b // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/sys v0.28.0 // indirect
 )
--- gokapi.go
+++ gokapi.go
@@ -8,12 +8,13 @@ import (
 	"strings"
 
 	"github.com/gokrazy/gokapi/ondeviceapi"
-	"github.com/gokrazy/gokrazy"
 )
 
 // ConnectOnDevice is meant to be run from a program running on a gokrazy
 // instance and establishes a connection to the gokrazy on-device API.
 func ConnectOnDevice() (*ondeviceapi.Configuration, error) {
+	const gokrazyUnixSocket = "/run/gokrazy-http.sock"
+
 	pw, err := os.ReadFile("/etc/gokr-pw.txt")
 	if err != nil {
 		return nil, err
@@ -23,13 +24,13 @@ func ConnectOnDevice() (*ondeviceapi.Configuration, error) {
 	cfg.BasePath = "http://" + auth + "@unix/"
 
 	// Use the Unix domain socket if available.
-	if conn, err := net.Dial("unix", gokrazy.HTTPUnixSocket); err == nil {
+	if conn, err := net.Dial("unix", gokrazyUnixSocket); err == nil {
 		conn.Close()
 		cfg.HTTPClient = &http.Client{
 			Transport: &http.Transport{
 				DialContext: func(ctx context.Context, _, _ string) (net.Conn, error) {
 					dialer := net.Dialer{}
-					return dialer.DialContext(ctx, "unix", gokrazy.HTTPUnixSocket)
+					return dialer.DialContext(ctx, "unix", gokrazyUnixSocket)
 				},
 			},
 		}
